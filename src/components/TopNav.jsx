3b627a7342175a4ad5ebc77f57f0ffc1a4310bae
nahid-hasan-web <nahidhasanniyan@gmail.com>
2025-10-16 19:59:13 +0600

    dropdown

--- src/components/TopNav.jsx
+++ src/components/TopNav.jsx
@@ -1,26 +1,33 @@
-import React, { useState, useEffect, useRef } from 'react';
+import React, { useState, useEffect, useRef } from "react";
 import { CiMicrophoneOn } from "react-icons/ci";
-import { RiSearch2Line } from 'react-icons/ri';
+import { RiLogoutCircleRLine, RiSearch2Line, RiSettingsLine } from "react-icons/ri";
+import { IoNotificationsOutline } from "react-icons/io5";
+import { FaRegUser } from "react-icons/fa";
+import { MdArrowDropUp } from "react-icons/md";
 
 const TopNav = () => {
   const [listening, setListening] = useState(false);
-  const [searchText, setSearchText] = useState('');
+  const [searchText, setSearchText] = useState("");
   const recognitionRef = useRef(null);
 
   useEffect(() => {
-    if (!('webkitSpeechRecognition' in window) && !('SpeechRecognition' in window)) {
-      alert('Your browser does not support Speech Recognition');
+    if (
+      !("webkitSpeechRecognition" in window) &&
+      !("SpeechRecognition" in window)
+    ) {
+      alert("Your browser does not support Speech Recognition");
       return;
     }
 
-    const SpeechRecognition = window.SpeechRecognition || window.webkitSpeechRecognition;
+    const SpeechRecognition =
+      window.SpeechRecognition || window.webkitSpeechRecognition;
     const recognition = new SpeechRecognition();
     recognition.continuous = true;
     recognition.interimResults = true;
-    recognition.lang = 'en-US';
-    
+    recognition.lang = "en-US";
+
     recognition.onresult = (event) => {
-      let transcript = '';
+      let transcript = "";
       for (let i = event.resultIndex; i < event.results.length; i++) {
         transcript += event.results[i][0].transcript;
       }
@@ -41,11 +48,12 @@ const TopNav = () => {
   };
 
   return (
-    <div className='w-full h-fit py-6 px-[50px] bg-white'>
-      <div className='search lg:w-[360px] h-[48px] rounded-[12px] bg-bgColor flex items-center justify-between py-[14px] px-[18px]'>
-        <RiSearch2Line className='text-xl text-[#7E7E8F]' />
+    <div className="w-full h-fit py-6 px-[50px] bg-white flex justify-between">
+      {/* ----------- search input */}
+      <div className="search lg:w-[360px] h-[48px] rounded-[12px] bg-bgColor flex items-center justify-between py-[14px] px-[18px]">
+        <RiSearch2Line className="text-xl text-[#7E7E8F]" />
         <input
-          className='flex-1 outline-none pl-2 text-[14px] font-poppins font-semibold text-[#C6CBD9]'
+          className="flex-1 outline-none pl-2 text-[14px] font-poppins font-semibold text-[#C6CBD9]"
           type="text"
           value={searchText}
           onChange={(e) => setSearchText(e.target.value)}
@@ -53,14 +61,31 @@ const TopNav = () => {
         />
         <button
           className={`w-8 h-8  text-xl flex justify-center items-center duration-[0.4s] ${
-            listening 
-              ? 'rounded-full bg-brandColor text-white animate-pulse'
-              : 'text-[#7E7E8F]'
+            listening
+              ? "rounded-full bg-brandColor text-white animate-pulse"
+              : "text-[#7E7E8F]"
           }`}
           onClick={toggleListening}
         >
           <CiMicrophoneOn />
         </button>
+      </div>
+      {/* ------------ profile datas */}
+      <div className="flex items-center gap-[48px]">
+        <button
+          className="text-xl text-secend"
+        >
+          <IoNotificationsOutline />
+        </button>
+        <button className="w-[48px] h-[48px] bg-brandColor rounded-full"></button>
+        <div className= " bg-white  rounded-[5px]  absolute top-[10%] right-10 shadow-[1px_0px_11px_0px_rgba(0,_0,_0,_0.1)] text-center">
+          <MdArrowDropUp className="text-3xl  absolute top-[-20px] text-brandColor left-[70px] " />
+
+
+          <button className="flex items-center  px-5 py-2 gap-3  hover:bg-brandColor hover:text-white duration-[.4s]"><FaRegUser />Profile</button>
+          <button className="flex items-center  px-5 py-2 gap-3  hover:bg-brandColor hover:text-white duration-[.4s]"><RiSettingsLine />Setting</button>
+          <button className="flex items-center  px-5 py-2 gap-3 hover:bg-brandColor hover:text-white duration-[.4s]"><RiLogoutCircleRLine />Logout</button>
+        </div>
       </div>
     </div>
   );

405f3498ef6e699fde27938fcd58e7764235eabc
nahid-hasan-web <nahidhasanniyan@gmail.com>
2025-10-17 15:46:03 +0600

    commo head added

--- src/components/common/CommonHead.jsx
+++ src/components/common/CommonHead.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const CommonHead = () => {
   return (
-    <div>CommonHead</div>
+    <h2 className='text-[28px] font-poppins font-bold text-[#07070C]'>CommonHead</h2>
   )
 }
 
--- src/pages/AddProductPage.jsx
+++ src/pages/AddProductPage.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
+import CommonHead from '../components/common/CommonHead'
 
 const AddProductPage = () => {
   return (
-    <>
+    <div className='mt-[42px] px-[23px]'>
+    <CommonHead/>
       AddProductPage
-    </>
+    </div>
   )
 }
 

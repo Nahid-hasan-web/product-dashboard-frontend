f0f2407d2eac9dc9d11bf45fcd50bad3b1e28521
nahid-hasan-web <nahidhasanniyan@gmail.com>
2025-10-18 13:17:15 +0600

    bread crumb added

---
+++ src/components/common/BreadCrumb.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import { MdKeyboardArrowRight } from 'react-icons/md'
+import { RiHome5Line } from 'react-icons/ri'
+import { Link } from 'react-router'
+
+const BreadCrumb = ({ pageName , pageLink}) => {
+  return (
+    <>
+        <div className='flex items-center gap-1 mt-3'>
+            <Link to={'/'}  className='flex items-center gap-1 text-sm font-normal font-poppins text-secend'><RiHome5Line />Home</Link>
+       <MdKeyboardArrowRight className='text-secend' />
+
+            <Link to={pageLink}  className='flex items-center gap-1 text-sm font-normal font-poppins text-brandColor'>{pageName}</Link>
+        </div>
+    </>
+  )
+}
+
+export default BreadCrumb
--- src/pages/AddProductPage.jsx
+++ src/pages/AddProductPage.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
 import CommonHead from '../components/common/CommonHead'
+import BreadCrumb from '../components/common/BreadCrumb'
 
 const AddProductPage = () => {
   return (
     <div className='mt-[42px] px-[23px]'>
     <CommonHead/>
-      AddProductPage
+      <BreadCrumb/>
     </div>
   )
 }

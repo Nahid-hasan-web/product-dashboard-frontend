82913d36ff22145fd5455549f220b47efd2a01d3
nahid-hasan-web <nahidhasanniyan@gmail.com>
2025-10-21 09:50:33 +0600

    product items added

--- src/pages/ProductPage.jsx
+++ src/pages/ProductPage.jsx
@@ -4,6 +4,7 @@ import CommonProductOptin from "../components/common/CommonProductOptin";
 import { BsBoxSeam } from "react-icons/bs";
 import BreadCrumb from "../components/common/BreadCrumb";
 import CommonHead from "../components/common/CommonHead";
+import { VscInbox } from "react-icons/vsc";
 
 const ProductPage = () => {
   return (
@@ -26,6 +27,34 @@ const ProductPage = () => {
             <BsBoxSeam className="text-6xl text-gray-400 transition-transform duration-500 group-hover:scale-125 group-hover:text-yellow-400" />
         }
         cardcontent={"Add Product"}
+        cardLink={'/addProduct'}
+        cardColor={'blue-100'}
+        />
+      </div>
+
+    <h2 className="text-2xl font-medium font-poppins text-secend mt-5 ">Items List</h2>
+
+       <div className="mt-5 flex gap-5 flex-wrap  ">
+        <CommonProductOptin
+          cardIcon={
+            <CiViewList className="text-6xl text-gray-400 transition-transform duration-500 group-hover:scale-125 group-hover:text-yellow-400" />
+        }
+        cardcontent={"View catagory"}
+        cardLink={'/'}
+        />
+        <CommonProductOptin
+          cardIcon={
+            <BsBoxSeam className="text-6xl text-gray-400 transition-transform duration-500 group-hover:scale-125 group-hover:text-yellow-400" />
+        }
+        cardcontent={"View Product"}
+        cardLink={'/addProduct'}
+        cardColor={'blue-100'}
+        />
+        <CommonProductOptin
+          cardIcon={
+            <VscInbox className="text-6xl text-gray-400 transition-transform duration-500 group-hover:scale-125 group-hover:text-yellow-400" />
+        }
+        cardcontent={"Manage Product"}
         cardLink={'/addProduct'}
         cardColor={'blue-100'}
         />

ef53b3a11e9f59fbbafe54857dcd9325dcd41b69
nahid-hasan-web <nahidhasanniyan@gmail.com>
2025-10-21 08:50:10 +0600

    produt  sub iamges upload added

--- src/components/PorductUploadBoard.jsx
+++ src/components/PorductUploadBoard.jsx
@@ -1,10 +1,13 @@
 import { useEffect, useState } from "react";
 import { IoMdArrowDropdown } from "react-icons/io";
 import { CiImageOn } from "react-icons/ci";
+import { RxCross1 } from "react-icons/rx";
 
 const PorductUploadBoard = () => {
   const [imgPath, setImgPath] = useState("");
+  const [subImage, setSubImage] = useState([]);
 
+  console.log(subImage);
   return (
     <>
       <div className=" flex-1 p-[50px] border border-[#E8EDF2]  bg-white rounded-[16px] mt-[18px]">
@@ -89,7 +92,11 @@ const PorductUploadBoard = () => {
           </div>
           {/* -------------- column 2 */}
           <div>
-            <div className={`w-full lg:w-[503px]   ${imgPath ?   "border h-fit" : "border-4 border-dashed h-[463px] "} border-borderColor  rounded-[8px]  flex justify-center items-center overflow-hidden`}>
+            <div
+              className={`w-full lg:w-[503px]   ${
+                imgPath ? "border h-fit" : "border-4 border-dashed h-[463px] "
+              } border-borderColor  rounded-[8px]  flex justify-center items-center overflow-hidden`}
+            >
               {imgPath ? (
                 <img src={imgPath} alt="thumbnail image" />
               ) : (
@@ -99,8 +106,8 @@ const PorductUploadBoard = () => {
                 >
                   <CiImageOn className="text-8xl text-brandColor" />
                   <h2 className="text-sm font-medium font-poppins text-secend">
-                    Drop your image here, or{" "}
-                    <span className="text-brandColor my-5">browse</span>
+                    Drop your image here, or
+                    <span className="text-brandColor my-5 ml-1">browse</span>
                   </h2>
                   <p className="text-[13px] font-normal font-poppins text-[#9A9AAF] mt-2">
                     JPG,PNG and GIF files are allowed
@@ -109,7 +116,7 @@ const PorductUploadBoard = () => {
                     onChange={(e) => {
                       const file = e.target.files[0];
                       if (file) {
-                        setImgPath(URL.createObjectURL(file)); // creates a temporary URL for preview
+                        setImgPath(URL.createObjectURL);
                       }
                     }}
                     className="invisible"
@@ -119,6 +126,58 @@ const PorductUploadBoard = () => {
                 </label>
               )}
             </div>
+            <h2 className="commonHead mt-[48px]">Product Sub-Images</h2>
+            <div
+              className={`w-full lg:w-[503px]   ${
+                imgPath ? "border h-fit" : "border-4 border-dashed h-[154px] "
+              } border-borderColor  rounded-[8px]  flex justify-center items-center overflow-hidden`}
+            >
+              <label
+                htmlFor="subimage"
+                className="flex flex-col justify-center items-center"
+              >
+                <CiImageOn className="text-5xl text-brandColor" />
+                <h2 className="text-sm font-medium font-poppins text-secend">
+                  Drop your image here, or
+                  <span className="text-brandColor my-5 ml-1">browse</span>
+                </h2>
+                <p className="text-[13px] font-normal font-poppins text-[#9A9AAF] mt-2">
+                  JPG,PNG and GIF files are allowed
+                </p>
+                <input
+                  onChange={(e) => {
+                    const file = e.target.files[0];
+                    if (file) {
+                      setSubImage((prev) => [
+                        ...prev,
+                        URL.createObjectURL(file),
+                      ]);
+                    }
+                  }}
+                  className="invisible"
+                  id="subimage"
+                  type="file"
+                />
+              </label>
+            </div>
+            {subImage.length > 0 && (
+              <div>
+                <h2 className="commonHead mt-[48px]">preview</h2>
+                <div className="flex gap-5 flex-wrap items-center mt-5">
+                  {subImage.map((item, i) => (
+                    <div
+                      key={i}
+                      className="w-[80px] h-[80px] rounded-[5px] bg-gray-100 relative group"
+                    >
+                      <img className="w-full" src={item} alt="sub images" />
+                      <button className="w-[20px] h-[20px] rounded-full bg-black flex justify-center items-center text-white text-sm absolute top-[-10px] right-0 duration-[.2s]  opacity-0 group-hover:opacity-100 active:scale-[1.1]">
+                        <RxCross1 />
+                      </button>
+                    </div>
+                  ))}
+                </div>
+              </div>
+            )}
           </div>
         </div>
       </div>

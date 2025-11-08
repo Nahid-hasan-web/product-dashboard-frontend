60cfeb29b33204695c58c05cd23f86eb257b2216
nahid-hasan-web <nahidhasanniyan@gmail.com>
2025-11-08 15:04:20 +0600

    loading added

--- src/pages/auth/Login.jsx
+++ src/pages/auth/Login.jsx
@@ -1,37 +1,47 @@
 import React, { useState } from "react";
-import { Link } from "react-router";
+import { Link, useNavigate } from "react-router";
 import { IoEyeOffOutline } from "react-icons/io5";
 import { IoEyeOutline } from "react-icons/io5";
 import axios from "axios";
 import tostifyMsg from "../../helpers/totstifyMsg";
-
+import { BeatLoader } from "react-spinners";
 const Login = () => {
-    const [showPass , setShowPass] = useState(false)
-    const [formData  ,setFormData] = useState({email:'' , password:''})
-    const [formError, setFormError] = useState({emailError :"border-gray-300 ",  passwordError:'border-gray-300 '})
+  const [showPass, setShowPass] = useState(false);
+  const [formData, setFormData] = useState({ email: "", password: "" });
+  const [formError, setFormError] = useState({
+    emailError: "border-gray-300 ",
+    passwordError: "border-gray-300 ",
+  });
+  const [loading, setLoading] = useState(false);
+  const navigate = useNavigate();
 
-    
-    const handelSubmit = async (e) => {
-  e.preventDefault();
+  const handelSubmit = async (e) => {
+    setLoading(true)
+    e.preventDefault();
 
-  // make sure formData is defined
-  if (!formData || typeof formData !== "object") return;
+    // make sure formData is defined
+    if (!formData || typeof formData !== "object") return;
 
-  if (!formData.email) {
-    setFormError((prev) => ({ ...prev, emailError: "border-red-500" }));
-  }
-  if (!formData.password) {
-    setFormError((prev) => ({ ...prev, passwordError: "border-red-500" }));
-    return;
-  }
-
-  try {
-    const res = await axios.post("http://localhost:8000/auth/login", formData);
-         tostifyMsg('sucess' , 'Login sucess')
-  } catch (err) {
-     tostifyMsg('error' , err.response.data)
-  }
-};
+    if (!formData.email) {
+      setFormError((prev) => ({ ...prev, emailError: "border-red-500" }));
+    }
+    if (!formData.password) {
+      setFormError((prev) => ({ ...prev, passwordError: "border-red-500" }));
+      return;
+    }
+    try {
+      const res = await axios.post(
+        "http://localhost:8000/auth/login",
+        formData
+      );
+      tostifyMsg("sucess", "Login sucess");
+      navigate("/");
+      setLoading(false);
+    } catch (err) {
+      tostifyMsg("error", err.response.data);
+      setLoading(false);
+    }
+  };
 
   return (
     <>
@@ -41,7 +51,7 @@ const Login = () => {
             Welcome back
           </h2>
 
-          <form  onSubmit={handelSubmit} className="space-y-4 sm:space-y-6">
+          <form onSubmit={handelSubmit} className="space-y-4 sm:space-y-6">
             <div>
               <label
                 htmlFor="email"
@@ -50,7 +60,13 @@ const Login = () => {
                 Email
               </label>
               <input
-                onChange={(e)=>{setFormData((prev)=>({...prev , email:e.target.value})) ; setFormError((prev)=>({...prev , emailError:'border-gray-300'}))}}
+                onChange={(e) => {
+                  setFormData((prev) => ({ ...prev, email: e.target.value }));
+                  setFormError((prev) => ({
+                    ...prev,
+                    emailError: "border-gray-300",
+                  }));
+                }}
                 type="email"
                 id="email"
                 className={`w-full px-3 sm:px-4 py-2.5 sm:py-3 rounded-lg border ${formError.emailError} focus:outline-none focus:ring-2 focus:ring-brandColor focus:border-transparent text-sm sm:text-base`}
@@ -61,35 +77,63 @@ const Login = () => {
             <div className=" relative">
               <label
                 htmlFor="password"
-                                className={`text-sm font-medium font-poppins text-secend block mb-1.5 sm:mb-2 `}
+                className={`text-sm font-medium font-poppins text-secend block mb-1.5 sm:mb-2 `}
               >
                 Password
               </label>
               <input
-                onChange={(e)=>{setFormData((prev)=>({...prev , password:e.target.value})) ; setFormError((prev)=>({...prev , passwordError:'border-gray-300'})) }}
-                type= {showPass? "text" : "password" }
+                onChange={(e) => {
+                  setFormData((prev) => ({
+                    ...prev,
+                    password: e.target.value,
+                  }));
+                  setFormError((prev) => ({
+                    ...prev,
+                    passwordError: "border-gray-300",
+                  }));
+                }}
+                type={showPass ? "text" : "password"}
                 id="password"
                 className={`w-full px-3 sm:px-4 py-2.5 sm:py-3  ${formError.passwordError} rounded-lg border border-gray-300 focus:outline-none focus:ring-2 focus:ring-brandColor focus:border-transparent text-sm sm:text-base`}
                 placeholder="Enter your password"
               />
-              {
-                showPass?
-                    <IoEyeOutline onClick={()=>setShowPass(!showPass)} className=" absolute bottom-4 text-secend right-3" />
-                        :
-                    <IoEyeOffOutline onClick={()=>setShowPass(!showPass)} className=" absolute bottom-4 text-secend right-3" />
-              }
-              
-              
-
+              {showPass ? (
+                <IoEyeOutline
+                  onClick={() => setShowPass(!showPass)}
+                  className=" absolute bottom-4 text-secend right-3"
+                />
+              ) : (
+                <IoEyeOffOutline
+                  onClick={() => setShowPass(!showPass)}
+                  className=" absolute bottom-4 text-secend right-3"
+                />
+              )}
             </div>
 
-            <Link className="text-[12px] block text-end mb-0  font-medium font-poppins text-brandColor" to={'/forgotPassword'}>forgot password?</Link>
-            <button
-              type="submit"
-              className="w-full active:scale-[1.1] duration-[4s] py-2.5 sm:py-3 mt-2 sm:mt-4 bg-brandColor text-white rounded-lg font-medium hover:bg-opacity-90 transition-colors text-sm sm:text-base"
+            <Link
+              className="text-[12px] block text-end mb-0  font-medium font-poppins text-brandColor"
+              to={"/forgotPassword"}
             >
-              Login
-            </button>
+              forgot password?
+            </Link>
+            {loading ?  (
+              <div
+                type="submit"
+                className="w-full text-center active:scale-[1.1] duration-[4s] py-2.5 sm:py-3 mt-2 sm:mt-4 bg-brandColor text-white rounded-lg font-medium hover:bg-opacity-90 transition-colors text-sm sm:text-base"
+              >
+                <BeatLoader size={10} color="#fff" />
+              </div>
+            )
+            :
+            (
+              <button
+                type="submit"
+                className="w-full active:scale-[1.1] duration-[4s] py-2.5 sm:py-3 mt-2 sm:mt-4 bg-brandColor text-white rounded-lg font-medium hover:bg-opacity-90 transition-colors text-sm sm:text-base"
+              >
+                Login
+              </button>
+            ) 
+          }
           </form>
 
           <p className="mt-4 sm:mt-6 text-center text-gray-600 text-sm sm:text-base">
